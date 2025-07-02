6b61d8c948b02c8a22c79672b7931501f1dd2a7c
Matteo Cipolla <mati_teo@live.it>
2025-07-02 09:37:07 +0200

    add database and types

---
+++ types.ts
@@ -0,0 +1,15 @@
+export type Smartphone = {
+  title: string;
+  category: string;
+  brand: string;
+  price: number;
+  screenSize: number;
+  storage: number;
+  ram: number;
+  battery: number;
+  camera: string;
+  os: string;
+  releaseYear: number;
+  description?: string;
+  image: string;
+};
